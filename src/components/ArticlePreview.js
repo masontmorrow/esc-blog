c325bf2bb4934c8af179e16dfb51e25506ef3c85
masontmorrow <masontmorrow@gmail.com>
2018-12-12 19:27:09 +0100

    Add featured_image & alt tag

--- src/components/ArticlePreview.js
+++ src/components/ArticlePreview.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'gatsby';
+import { Link } from 'gatsby'
+import Img from 'gatsby-image'
 
 const ArticlePreview = ({post}) => {
     console.log(post);
+    const featured_image = post.frontmatter.hasOwnProperty('featured_image');
     return (
         <div className="article-preview">
-            <div><img src="" alt=""/></div>
+            <img 
+                src={featured_image ? `${post.frontmatter.featured_image.publicURL}` : ""} 
+                alt={featured_image ? `${post.frontmatter.featured_image_alt}` : ""}
+            />
           <p>
                <Link className="" to={post.fields.slug}>
                {post.frontmatter.title}
