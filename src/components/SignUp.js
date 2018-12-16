52020e28ca4e2b5cb255cea76997bfe127c0e6e4
masontmorrow <masontmorrow@gmail.com>
2018-12-16 20:06:18 +0100

    Add fetch API to handle user subscribe (not working):
    - Mailchimp API does not support client-side fetch API

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,20 +1,48 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+
 class SignUp extends React.Component {
     state = {
-        email: "",
+        email_address: "",
         gdpr_8849: false,
-        gdpr_8857: false
+        gdpr_8857: false,
+        b_b6d912c6cb50482e3f936b7d6_ae758c88fd: ""
     }
 
     onChange = e => {
         this.setState({ [e.target.name]: e.target.value });
     }
 
-    submitEmail = e => {
+    submitEmail = (e, data) => {
         e.preventDefault();
-
+        data.status = 'subscribed'; // 'subscribed' - no confirm email, 'pending' - confirmation email sent 
+        const email = data.gdpr_8849;
+        const marketing = data.gdpr_8857;
+        email ? data.marketing_permissions = [ 
+            {
+                marketing_permission_id: "0c55d68885",
+                enabled: true
+            }
+        ] : data.marketing_permissions = [];
+        marketing ? data.marketing_permissions.push(
+            {
+                marketing_permission_id: "c9abdd901a",
+                enabled: true
+            }
+        ) : data.marketing_permissions = data.marketing_permissions;
+        console.log(e, data);
+        fetch(`${process.env.REACT_APP_MAILCHIMP_API}/lists/${process.env.REACT_APP_MAILCHIMP_LIST_ID}/members`, {
+            method: 'POST',
+            headers: {
+                'Authorization': `apikey ${process.env.REACT_APP_MAILCHIMP_KEY}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(json => console.log(json))
+        .catch(error => console.error('Error:', error));
     }
 
     toggle = e => {
@@ -23,17 +51,22 @@ class SignUp extends React.Component {
     }
 
     render() {
-        const { email, gdpr_8849, gdpr_8857 } = this.state;
+        const {
+            email_address,
+            gdpr_8849,
+            gdpr_8857,
+            b_b6d912c6cb50482e3f936b7d6_ae758c88fd
+        } = this.state;
         return (
             <section className="signup">
                 <div>
                     <p>This is a summary of our product and service offering.</p>
                 </div>
                 <div>
-                    <form action ="https://antoniotgraniero.us7.list-manage.com/subscribe/post?u=b6d912c6cb50482e3f936b7d6&amp;id=ae758c88fd" method="post">
+                    <form>
                         <p>Subscribe to our mailing list.</p>
-                        <label htmlFor="email" className="anton">
-                            <input type="email" name="email" onChange={this.onChange} value={email} id="mce-EMAIL"/>
+                        <label htmlFor="email_address" className="anton">
+                            <input type="email" name="email_address" onChange={this.onChange} value={email_address} id="mce-EMAIL"/>
                             <span>email</span>
                         </label>
                         {/* Hidden fields below */}
@@ -50,9 +83,9 @@ class SignUp extends React.Component {
                             <p>We use Mailchimp as our marketing platform. By clicking below to subscribe, you acknowledge that your information will be transferred to Mailchimp for processing.</p>
                             <a href="https://mailchimp.com/legal/" target="_blank" rel="noopener noreferrer">Learn more about Mailchimp's privacy practices here.</a>
                             <div style={{ position: "absolute", left: "-5000px", ariaHidden: "true"}}>
-                                <input type="text" name="b_b6d912c6cb50482e3f936b7d6_ae758c88fd" tabIndex="-1" value="" onChange={this.onChange}/>
+                                <input type="text" name="b_b6d912c6cb50482e3f936b7d6_ae758c88fd" tabIndex="-1" value={b_b6d912c6cb50482e3f936b7d6_ae758c88fd} onChange={this.onChange}/>
                             </div>
-                            <button className="button" type="submit" onClick={this.submitEmail}>iscriviti</button>
+                            <button className="button" type="submit" onClick={e => this.submitEmail(e, this.state)}>iscriviti</button>
                         </div>
                     </form>
                 </div>
